b2ab9114e55d2f4dc4df90d1fda5f6731ae0ad7f
hiaoxui <hiaoxui@gmail.com>
2024-12-09 15:39:49 -0500

    add tz

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='walless_utils',
-    version='2.3.0',
+    version='2.3.1',
     packages=['walless_utils', 'walless_utils.objects', 'walless_utils.api', 'walless_utils.global_obj'],
     url='',
     license='',
